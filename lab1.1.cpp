9228601e4e30efaef6f58dcdae17918d711d0687
Rigoberto Lomas Velazco <rlomasvelazco001@student.butte.edu>
2016-08-31 17:43:00 +0000

    teacher

---
+++ lab1.1.cpp
@@ -0,0 +1,14 @@
+/* Lab 1 ASCII Art
+Rigoberto Lomas-Velazco
+8/22/16*/
+
+# include <iostream>
+using namespace std;
+int main ()
+{
+    cout<< "RRRR   IIIII GGGGGG OOOOO" << endl;
+    cout<< "R   R    I   G      O   O" << endl;
+    cout<< "RRRR     I   GGGGGG O   O" << endl;
+    cout<< "R   R    I   G  G G O   O" << endl;
+    cout<< "R   R  IIIII GGGG G OOOOO" << endl;
+}
