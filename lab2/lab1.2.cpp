dd61580f646649ee1d8f456eedd23b5198e83686
Rigoberto Lomas Velazco <rlomasvelazco001@student.butte.edu>
2016-09-07 19:19:17 +0000

    Lab 2 submission

---
+++ lab2/lab1.2.cpp
@@ -0,0 +1,12 @@
+/* Lab2 Algorithm
+Rigoberto Lomas-Velazco
+8/22/16*/
+
+/* program chooses a random number "x" between 1 and 10.
+  player must then guess a number "y" in an attempt to make "y=x"
+  if "y≠x", then the program will tell the player they were wrong and will lose 1 point of a possible 10
+  if "y<x", then the program will tell the player to guess higher
+  if "y>x", then the program will tell the player to guess lower
+  this process will repeat until the player guesses the correct number "y" so that "y=x"
+  if the player chooses the correct number "y", then the plyer will recieve "10-z" points
+  the varible "z" represents the number of times the player guessed incorrectly */
